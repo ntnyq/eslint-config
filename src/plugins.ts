7ea9a11601022f3e9621230990a381d403ff77d9
ntnyq <ntnyq13@gmail.com>
2024-04-17 14:22:19 +0800

    feat: replace plugin import with import-x

--- src/plugins.ts
+++ src/plugins.ts
@@ -4,11 +4,8 @@
 
 // @ts-nocheck
 
-export * as pluginYaml from 'eslint-plugin-yml'
 export { default as pluginNode } from 'eslint-plugin-n'
-export { default as pluginImport } from 'eslint-plugin-i'
 export { default as pluginVue } from 'eslint-plugin-vue'
-export { default as pluginJsonc } from 'eslint-plugin-jsonc'
 export { default as pluginUnoCSS } from '@unocss/eslint-plugin'
 export { default as pluginUnicorn } from 'eslint-plugin-unicorn'
 export { default as pluginPrettier } from 'eslint-plugin-prettier'
@@ -16,7 +13,12 @@ export { default as pluginMarkdown } from 'eslint-plugin-markdown'
 export { default as pluginComments } from 'eslint-plugin-eslint-comments'
 export { default as pluginTs } from '@typescript-eslint/eslint-plugin'
 
-export { default as parserYaml } from 'yaml-eslint-parser'
-export { default as parserVue } from 'vue-eslint-parser'
-export { default as parserJsonc } from 'jsonc-eslint-parser'
+export * as pluginYaml from 'eslint-plugin-yml'
+export * as pluginJsonc from 'eslint-plugin-jsonc'
+export * as pluginImport from 'eslint-plugin-import-x'
+
+export * as parserYaml from 'yaml-eslint-parser'
+export * as parserVue from 'vue-eslint-parser'
+export * as parserJsonc from 'jsonc-eslint-parser'
+
 export * as parserTs from '@typescript-eslint/parser'
