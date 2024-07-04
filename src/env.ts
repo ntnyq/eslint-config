2e76e372f83461f217a31bf32f5e80c08d63894a
ntnyq <ntnyq13@gmail.com>
2024-07-05 06:18:57 +0800

    feat: check env in playground dir

--- src/env.ts
+++ src/env.ts
@@ -1,3 +1,5 @@
+import process from 'node:process'
+import { resolve } from 'node:path'
 import { isPackageExists } from 'local-pkg'
 
 export const hasTypeScript = isPackageExists('typescript')
@@ -6,7 +8,16 @@ export const hasVue =
   isPackageExists('vue') ||
   isPackageExists('nuxt') ||
   isPackageExists('vitepress') ||
-  isPackageExists('@slidev/cli')
+  isPackageExists('@slidev/cli') ||
+  isPackageExists('vue', {
+    paths: [resolve(process.cwd(), 'playground')],
+  })
 
 export const hasUnoCSS =
-  isPackageExists('unocss') || isPackageExists('@unocss/webpack') || isPackageExists('@unocss/nuxt')
+  isPackageExists('unocss') ||
+  isPackageExists('unocss', {
+    paths: [resolve(process.cwd(), 'playground')],
+  }) ||
+  isPackageExists('@unocss/postcss') ||
+  isPackageExists('@unocss/webpack') ||
+  isPackageExists('@unocss/nuxt')
