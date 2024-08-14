d7ff3b0500df4c2dc27dc226379fd6ac49913268
ntnyq <ntnyq13@gmail.com>
2024-08-14 17:54:49 +0800

    fix: cleanup `eslint-config-prettier`

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,11 +3,13 @@ import { ntnyq } from './dist/index.js'
 export default ntnyq(
   [
     {
+      name: 'ntnyq/workspace/ignore',
       ignores: ['**/types/typegen.ts'],
     },
   ],
   {
     vue: true,
     unocss: true,
+    vitest: true,
   },
 )
--- src/shims.d.ts
+++ src/shims.d.ts
@@ -4,8 +4,7 @@
 
 declare module '@eslint/js'
 
-declare module 'eslint-config-prettier'
-
 declare module 'eslint-plugin-vue'
 declare module 'eslint-plugin-markdown'
+
 declare module '@eslint-community/eslint-plugin-eslint-comments'
