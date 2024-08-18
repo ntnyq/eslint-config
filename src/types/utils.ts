b87c77ab4b08349c5995a1fb0340206f8d0a3561
ntnyq <ntnyq13@gmail.com>
2024-08-18 11:36:31 +0800

    chore: cleanup types `OverridesOptions`

--- src/types/utils.ts
+++ src/types/utils.ts
@@ -2,18 +2,10 @@
  * @file Type Utils
  */
 
-import type { ParserOptions, TypedConfigItem } from './eslint'
-
 export type Arrayable<T> = T | T[]
 
 export type Awaitable<T> = T | Promise<T>
 
 export type InteropModuleDefault<T> = T extends { default: infer U } ? U : T
-
-export interface OverridesOptions<Rules = TypedConfigItem['rules']> {
-  files?: TypedConfigItem['files']
-  rules?: Rules
-  parserOptions?: ParserOptions
-}
 
 export type ResolvedOptions<T> = T extends boolean ? never : NonNullable<T>
