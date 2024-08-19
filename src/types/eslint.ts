b21ca137107a8977192bd2d23b654714d04d2322
ntnyq <ntnyq13@gmail.com>
2024-08-20 00:31:46 +0800

    fix: cleanup moot type `Rules`

--- src/types/eslint.ts
+++ src/types/eslint.ts
@@ -2,21 +2,16 @@ import type { RuleOptions } from './typegen'
 import type { Linter } from 'eslint'
 
 /**
- * Parser options
- */
-export type ParserOptions = Linter.ParserOptions
-
-/**
- * Rules
- */
-export type Rules = RuleOptions
-
-/**
  * Typed flat config item
  */
-export type TypedConfigItem = Omit<Linter.Config<Linter.RulesRecord & Rules>, 'plugins'> & {
+export type TypedConfigItem = Omit<Linter.Config<Linter.RulesRecord & RuleOptions>, 'plugins'> & {
   /**
    * Most plugin are not properly typed
    */
   plugins?: Record<string, any>
 }
+
+/**
+ * Parser options
+ */
+export type ParserOptions = Linter.ParserOptions
