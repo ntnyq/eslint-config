fb4b2618cbf85d8dd62e67748587b0b96e1e7c1a
ntnyq <ntnyq13@gmail.com>
2025-01-11 13:43:47 +0800

    chore: cleanup `unstable_ts_config` flag

--- src/configs/svgo.ts
+++ src/configs/svgo.ts
@@ -1,9 +1,18 @@
-import { config as createSVGOConfig } from 'eslint-plugin-svgo'
+import { createConfig as createSVGOConfig } from 'eslint-plugin-svgo'
+import { GLOB_SVG } from '../globs'
 import type { ConfigSVGOOptions, TypedConfigItem } from '../types'
 
-export const svgo = (options: ConfigSVGOOptions = {}): TypedConfigItem[] => [
-  {
-    ...createSVGOConfig(options),
-    name: 'ntnyq/svgo',
-  },
-]
+export const svgo = (options: ConfigSVGOOptions = {}): TypedConfigItem[] => {
+  const { files = [GLOB_SVG], rules: overridesRules = {} } = options
+  return [
+    createSVGOConfig({
+      name: 'ntnyq/svgo',
+      files,
+      rules: {
+        'svgo/svgo': 'error',
+
+        ...overridesRules,
+      },
+    }),
+  ]
+}
